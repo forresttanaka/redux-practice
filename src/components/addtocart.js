e662165f61bc4f5b7f6675200487787abc0b385f
Forrest Tanaka <fytanaka@stanford.edu>
2018-03-12 10:20:20 -0700

    Add proptypes to addtocart

--- src/components/addtocart.js
+++ src/components/addtocart.js
@@ -1,10 +1,20 @@
 import React from "react";
+import PropTypes from 'prop-types';
 import { connect } from "react-redux";
 import { addToCart } from "../reducers/cartreducer";
 
+// Button to add the current experiment to the cart.
 let AddToCart = ({ onAddToCartClick }) => (
   <button onClick={onAddToCartClick}>Add to Cart</button>
 );
+
+AddToCart.propTypes = {
+  onAddToCartClick: PropTypes.func, //  Function to call when Add to Cart clicked
+};
+
+AddToCart.defaultProps = {
+  onAddToCartClick: null,
+};
 
 const mapDispatchToProps = (dispatch, ownProps) => (
   {
